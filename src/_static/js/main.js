a1b5a52dd0ea16bc319ada7f1daa6691c05f7d93
rahul1115 <rahulgupta.ra.com@gmail.com>
2022-07-16 16:27:22 +0530

    chnages

--- src/_static/js/main.js
+++ src/_static/js/main.js
@@ -9,8 +9,15 @@ var h =
   document.documentElement.clientHeight ||
   document.body.clientHeight;
 
+
+const menu = document.querySelectorAll('nav ul li');
+const underline = document.querySelector('.underline');
+const navUl = document.querySelector('nav ul');
+const menuToggle = document.querySelector('.menu-toggle');
+
 document.addEventListener('DOMContentLoaded', () => {
   splideInit();
+  menuInteractive();
 });
 
 document.onscroll = () => {
@@ -18,33 +25,38 @@ document.onscroll = () => {
 
 window.onload = () => {}
 
-
-const menu = document.querySelectorAll('nav ul li');
-const underline = document.querySelector('.underline');
-const navUl = document.querySelector('nav ul');
-const menuToggle = document.querySelector('.menu-toggle');
-
 function underlineLink() {
-  const linkCoords = this.getBoundingClientRect();
-  underline.style.transform = `translate(${linkCoords.left + window.scrollX}px, -3px)`;
-  underline.style.borderBottomWidth = "3px";
-  underline.style.width = `${linkCoords.width}px`;
-  underline.style.opacity = "100";
+  try {
+    const linkCoords = this.getBoundingClientRect();
+    underline.style.transform = `translate(${linkCoords.left + window.scrollX}px, -3px)`;
+    underline.style.borderBottomWidth = "3px";
+    underline.style.width = `${linkCoords.width}px`;
+    underline.style.opacity = "100";
+  } catch (error) {
+    if(showlog) console.log(error);
+  }
 }
 
 function removeLink() {
-  underline.style.opacity = "0";
+  try {
+    underline.style.opacity = "0";
+  } catch (error) {
+    if(showlog) console.log(error)    
+  }
 }
 
 function menuExpand() {
-  navUl.classList.toggle("h-auto");
-  document.querySelector('.bar2').classList.toggle("opacity-0");
-  document.querySelector('.bar1').classList.toggle("-rotate-45");
-  document.querySelector('.bar1').classList.toggle("translate-y-px");
-  document.querySelector('.bar3').classList.toggle("-translate-y-px");
-  document.querySelector('.bar3').classList.toggle("rotate-45");
+  try {
+    navUl.classList.toggle("h-auto");
+    document.querySelector('.bar2').classList.toggle("opacity-0");
+    document.querySelector('.bar1').classList.toggle("-rotate-45");
+    document.querySelector('.bar1').classList.toggle("translate-y-px");
+    document.querySelector('.bar3').classList.toggle("-translate-y-px");
+    document.querySelector('.bar3').classList.toggle("rotate-45");
+  } catch (error) {
+    if(showlog) console.log(error);
+  }
 }
-
 
 function splideInit(){
   try {
@@ -56,9 +68,16 @@ function splideInit(){
       preLoadPages: 0,
     }).mount();
   } catch (error) {
+    if (showlog) console.log(error);
   }
 }
 
-menu.forEach(link => link.addEventListener('mouseenter', underlineLink));
-navUl.addEventListener('mouseleave', removeLink);
-menuToggle.addEventListener('click', menuExpand);
+function menuInteractive() {
+  try {
+    menu.forEach(link => link.addEventListener('mouseenter', underlineLink));
+    navUl.addEventListener('mouseleave', removeLink);
+    menuToggle.addEventListener('click', menuExpand);
+  } catch (error) {
+    if(showlog) console.log(error);
+  }
+}
